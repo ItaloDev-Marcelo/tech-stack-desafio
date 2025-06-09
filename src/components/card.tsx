4ca83ff1fe4239082a01e1c0efe5cf120f148425
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-06-09 11:41:28 -0300

    atualizando dados e estilo

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,8 +7,9 @@ type Props  = {
 
 export default function Card({bubbleTitle, username, position}: Props) {
     return (
-        <article className="full-w flex justify-start content-center items-center  h-auto md:w-[250px] lg:w-[300px] 
-         rounded-[7px]  bg-white my-5 lg:my-[0.5em] p-4">
+        <article className="full-w flex justify-start content-center items-center  
+        h-[100px] md:w-[250px] lg:w-[300px] 
+         rounded-[7px] md:m-2  bg-white my-5 lg:my-[0.2em] p-4 shadow">
              <div className="w-[50px] h-[50px] mr-2 rounded-[50px] cursor-pointer
               hover:text-amber-200
               bg-gray-300 text-center flex-col justify-center content-center ">
